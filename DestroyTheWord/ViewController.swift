e6ce475456beb3ddfb496b6ddfce4dc7cb37de07
Mac <Mac@xxxxxxs-MacBook-Pro.local>
2017-10-28 16:50:41 -0700

    Initial Commit

---
+++ DestroyTheWord/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DestroyTheWord
+//
+//  Created by Mac on 28/10/2017.
+//  Copyright © 2017 Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
